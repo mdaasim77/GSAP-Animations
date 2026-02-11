709283c69796dd0d1ff61cb65f15484ac9248db2
mdaasim77 <khanaasim9910@gmail.com>
2026-02-11 11:44:39 +0530

    learn to move in x axis and use stagger

--- Script.js
+++ Script.js
@@ -1,1 +1,17 @@
-console.log("Hellow World !");
+console.log("Aasim is here !");
+
+gsap.to("#box", {
+  x: 1000,
+  duration: 3,
+  delay: 1,
+  marginTop: "10px",
+  borderRadius: "50%",
+});
+
+gsap.from("h1", {
+  duration: 1,
+  delay: 1,
+  y: 100,
+  opacity: 0, // using gsap.from so its running opacity 0 to 1
+  stagger: 0.4,
+});
