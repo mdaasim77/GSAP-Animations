8e6c270f5581612fd1681baf3d733290cf037005
mdaasim77 <khanaasim9910@gmail.com>
2026-02-11 13:12:56 +0530

    working on GSAP using timeline and stagger

--- Script.js
+++ Script.js
@@ -16,44 +16,45 @@ gsap.from("h1", {
   stagger: 0.4,
 });
 
-gsap.to("p",{
-    duration:4,
-    x:"1070",
-    // delay:1,
-    repeat:-1,  // repeat -1 is used to repeat item infinite times
-    yoyo:true // yoyo property can used to get bounce the item
-})
+gsap.to("p", {
+  duration: 4,
+  x: "1070",
+  // delay:1,
+  repeat: -1, // repeat -1 is used to repeat item infinite times
+  yoyo: true, // yoyo property can used to get bounce the item
+});
 
-gsap.to("#ball",{
-    y:[200,0],
-    x:[400,800],
-    duration:2,
-    repeat:-1,
-    yoyo:true
-})
-
+gsap.to("#ball", {
+  y: [200, 0],
+  x: [400, 800],
+  duration: 2,
+  repeat: -1,
+  yoyo: true,
+});
 
 // ============== time line property
 
-var tl = gsap.timeline()
+var tl = gsap.timeline();
 
-tl.to(".timeline1",{
-    x:800,
-    y:-400,
-    // delay:1,
-    duration:2
-})
-tl.to(".timeline2",{
-    x:800,
-    y:-300,
-    // delay:1,
-    duration:2,
-    backgroundColor:"orange"
-})
-tl.to(".timeline3",{
-    x:800,
-    y:-200,
-    // delay:1,
-    duration:2,
-    borderRadius:20
-})
+tl.to(".timeline1", {
+  x: 800,
+  y: -400,
+  // delay:1,
+  duration: 2,
+  backgroundColor: "orange"
+});
+tl.to(".timeline2", {
+  x: 800,
+  y: -300,
+  // delay:1,
+  duration: 2,
+  backgroundColor: "white",
+ 
+});
+tl.to(".timeline3", {
+  x: 800,
+  y: -200,
+  // delay:1,
+  duration: 2,
+  borderRadius: 20,
+});
