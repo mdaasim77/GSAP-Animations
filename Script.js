7de61cdd3e33fb5d469751dc87996f071d933c6c
mdaasim77 <khanaasim9910@gmail.com>
2026-02-11 12:24:03 +0530

    in this we learn yoyo and repeat

--- Script.js
+++ Script.js
@@ -15,3 +15,21 @@ gsap.from("h1", {
   opacity: 0, // using gsap.from so its running opacity 0 to 1
   stagger: 0.4,
 });
+
+gsap.to("p",{
+    duration:4,
+    x:"1070",
+    // delay:1,
+    repeat:-1,  // repeat -1 is used to repeat item infinite times
+    yoyo:true // yoyo property can used to get bounce the item
+})
+
+gsap.to("#ball",{
+    y:[200,0],
+    x:[400,800],
+    duration:2,
+    repeat:-1,
+    yoyo:true
+
+
+})
