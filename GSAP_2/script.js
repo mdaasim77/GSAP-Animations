3bd6504d0f136d8d17d59fb891fc10b518aabb01
mdaasim77 <khanaasim9910@gmail.com>
2026-02-11 16:05:11 +0530

    trigger animation

---
+++ GSAP_2/script.js
@@ -0,0 +1,21 @@
+gsap.from("#page1 #box", {
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+  repeat:-1,
+  yoyo:true
+});
+gsap.from("#page2 #box", {
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+  repeat:-1,
+  yoyo:true
+});
+gsap.from("#page3 #box", {
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+  repeat:-1,
+  yoyo:true
+});
