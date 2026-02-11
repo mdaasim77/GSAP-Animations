dbb424ab4625686da27f6793bf0185487d191086
mdaasim77 <khanaasim9910@gmail.com>
2026-02-11 19:01:16 +0530

    working on

--- GSAP_2/script.js
+++ GSAP_2/script.js
@@ -1,21 +1,17 @@
 gsap.from("#page1 #box", {
   duration: 2,
-  delay: 1,
+  //   delay: 1,
   rotate: 360,
-  repeat:-1,
-  yoyo:true
 });
 gsap.from("#page2 #box", {
   duration: 2,
-  delay: 1,
+  //   delay: 1,
   rotate: 360,
-  repeat:-1,
-  yoyo:true
+  scrollTrigger: "#page2 #box",
 });
 gsap.from("#page3 #box", {
   duration: 2,
-  delay: 1,
+  //   delay: 1,
   rotate: 360,
-  repeat:-1,
-  yoyo:true
+  scrollTrigger: "#page3 #box",
 });
