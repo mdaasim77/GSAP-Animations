d7a2d728ed7c0be504de19f87078e038144ca354
mdaasim77 <khanaasim9910@gmail.com>
2026-02-12 11:51:39 +0530

    understand scrolltrigger property

--- GSAP_2/script.js
+++ GSAP_2/script.js
@@ -7,7 +7,11 @@ gsap.from("#page2 #box", {
   duration: 2,
   //   delay: 1,
   rotate: 360,
-  scrollTrigger: "#page2 #box",
+  scrollTrigger: {
+    trigger: "#page2 #box",
+    markers: true,
+    start: "top 50%",
+  },
 });
 gsap.from("#page3 #box", {
   duration: 2,
