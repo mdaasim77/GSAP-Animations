2468662a1ff4546d3c75563cc9ea0274563d39cc
mdaasim77 <khanaasim9910@gmail.com>
2026-02-12 13:23:03 +0530

    understand scrolltrigger

--- GSAP_2/script.js
+++ GSAP_2/script.js
@@ -1,51 +1,65 @@
-// gsap.from("#page1 .box", {
-//   duration: 2,
-//   //   delay: 1,
-//   rotate: 360,
-// });
-// gsap.from("#page2 .box", {
-//   duration: 2,
-//   //   delay: 1,
-//   rotate: 360,
-//   scrollTrigger: {
-//     trigger: "#page2 .box",
-//     start: "top 80%",
-//     end: "top 30%",
-//     scrub: true,
-//     markers: true,
-//   },
-// });
-// gsap.from("#page3 .box", {
-//   duration: 2,
-//   //   delay: 1,
-//   rotate: 360,
-//   scrollTrigger: "#page3 .box",
-// });
+// gsap.registerPlugin(ScrollTrigger);
 
-// ====== new tl code
+gsap.from("#page1 .box", {
+  duration: 2,
+  //   delay: 1,
+  rotate: 360,
+});
 
-var tll = gsap.timeline();
+gsap.from("#page2 .box", {
+  duration: 2,
+  rotate: 360,
+  opacity: 0,
+  // delay:1,
+  scrollTrigger: {
+    trigger: "#page2",
+    start: "top 50%",
+    end: "top 10%",
+    scrub: true,
+    markers: true,
+  },
+});
 
-tll
-  .from("h1", {
-    y: 100,
-    opacity: 0,
-    duration: 2,
-  })
-  .from(
-    "p",
-    {
-      y: 100,
-      opacity: 0,
-      duration: 2,
-    },
-    "-=1.3",
-  )
-  .from(
-    "button",
-    {
-      scale: 0,
-      duration: 1,
-    },
-    "<",
-  );
+gsap.from("#page3 .box", {
+  duration: 2,
+  //   delay: 1,
+  rotate: 360,
+  scrollTrigger: "#page3 .box",
+});
+
+// // ====== ================ = =====.    ==== == = = = = = = = =.  =. =new tl code
+
+// var tll = gsap.timeline();
+
+// tll
+//   .from("h1", {
+//     y: 100,
+//     opacity: 0,
+//     duration: 2,
+//   })
+//   .from(
+//     "p",
+//     {
+//       y: 100,
+//       opacity: 0,
+//       duration: 2,
+//     },
+//     "-=1.3",
+//   )
+//   .from(
+//     "button",
+//     {
+//       scale: 0,
+//       duration: 1,
+//     },
+//     "<",
+//   );
+
+// =========== =========== ============ ========== =========
+
+// const tl = gsap.timeline();
+
+// tl.add("intro")
+//   .from("h1", { y: 100, duration: 1 }, "intro")
+//   .from("p", { y: 100, duration: 1 }, "intro+=0.3")
+//   .from("button", { scale: 0, duration: 1 }, "intro+=0.5");
